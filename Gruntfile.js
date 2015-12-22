b3185dac4f055c1aed67fd058e6b200622850947
octavianrotari <octavianrotari@gmail.com>
2015-12-22 16:41:29 +0000

    grunt uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,17 @@ module.exports = function(grunt) {
       },
 
       build: ['Gruntfile.js', 'src/**/*.js']
+    },
+
+    uglify: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'dist/js/magic.min.js': ['src/js/magic.js', 'src/js/magic.js']
+        }
+      }
     }
 
   });
